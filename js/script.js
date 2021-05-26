9bb2ada22281440c80c917112577495d28a15830
julia2017-yg <julka.getman@gmail.com>
2021-05-26 16:53:00 +0300

    feat: add style

--- js/script.js
+++ js/script.js
@@ -11,13 +11,35 @@ const iconMenu = document.querySelector('.menu__icon');
 
 ///////////accordeon///////////////
 
-const tipsLink = document.querySelectorAll('.tips__link');
-const tipsSub = document.querySelectorAll('.tips-sub');
+const tipLink = document.querySelectorAll('.tips__link');
+const tipSub = document.querySelectorAll('.block-text');
 
-for (let i = 0; i < tipsLink.length; i++) {
-  tipsLink[i].addEventListener("click", () => {
-    tipsSub[i].classList.toggle('hidden');
-    tipsLink[i].classList.toggle('tips__link_active');
+for (let i = 0; i < tipLink.length; i++) {
+  tipLink[i].addEventListener("click", () => {
+    tipSub[i].classList.toggle('hidden');
+    tipLink[i].classList.toggle('tips__link_active');
   })
 }
 
+const fqasLink = document.querySelectorAll('.fqas__link');
+const tipSubElem = document.querySelectorAll('.block-text-next');
+
+ for (let i = 0; i < fqasLink.length; i++) {
+  fqasLink[i].addEventListener("click", () => {
+    tipSubElem[i].classList.toggle('hidden');
+    fqasLink[i].classList.toggle('fqas__link_active');
+   })
+ }
+
+// fqasLink.forEach((btn, index) => {
+// btn.addEventListener('click', () => {
+// fqaSub.forEach((subElem) => {
+//        subElem.classList.add('hidden');      });
+    
+//    fqasLink.forEach((fqaElem) => {
+//     fqaElem.classList.remove('fqas__link_active');
+//   });
+//   fqaSub[index].classList.remove('hidden');
+//   btn.classList.add('fqas__link_active');
+//   });
+//  });
