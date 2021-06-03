cfe90634af80d8c358d292e510ee51f266125a1a
julia2017-yg <julka.getman@gmail.com>
2021-06-03 13:48:26 +0300

    create slider

--- homework-2/js/main.js
+++ homework-2/js/main.js
@@ -14,3 +14,18 @@ window.addEventListener("scroll", function() {
     var header = document.querySelector("header");
     header.classList.toggle("sticky-header", window.scrollY > 0);
 });
+
+$(document).ready(function(){ 
+  $('.slider_inner').slick({
+    dots: true,
+    dotsClass: "my-dots",
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+  });
+          
+ 
+});
