87d37e82a6015b1e7cbe921a8c06da54533bb7a8
julia2017-yg <julka.getman@gmail.com>
2021-07-26 09:39:03 +0300

    add new lie

--- JS tasks/task cash/script.js
+++ JS tasks/task cash/script.js
@@ -1,37 +1,75 @@
-const getBtn = document.querySelector('.get-btn');
-let limits = { 1000: 5, 500: 2, 100: 5, 50: 100, 30: 6 };
+function giveMyMoney(money, coins) {
+  let needCoins = [], minCoins = []; minCoins[0] = 0;
 
-let iWantToGet = (ammountRequired, limits) => {
+  // we sort out all the amounts up to a given amount (you can start with a min bill)
+  for (let sum = 1; sum <= money; sum++) {     
 
-  function collect(amount, nominals) {
-    if (amount === 0) return {}; // success
+      // по умолчанию эта сумма не для выдачи
+      minCoins[sum] = Number.MAX_VALUE;
 
-    if (!nominals.length) return; // failure
+      // by default this amount is not for issue
+      for (let coin = coins.length - 1; coin >= 0; coin--) {
+          if (sum >= coins[coin].val && minCoins[sum] > minCoins[sum - coins[coin].val] + 1) {
 
-    let currentNominal = nominals[0];
-    let avaiableNotes = limits[currentNominal];
-    let notesNeeded = Math.floor(amount / currentNominal);
-    let numberOfNotes = Math.min(avaiableNotes, notesNeeded);
-
-    for (let i = numberOfNotes; i >= 0; i--) {
-      
-      let result = collect(amount - i * currentNominal, nominals.slice(1));
-
-      if (result) {
-        return i ? { [currentNominal]: i, ...result } : result;
+              // found a way to issue the amount sum
+              minCoins[sum] = minCoins[sum - coins[coin].val] + 1;
+          }
       }
-    }
-    
   }
 
-  let nominals = Object.keys(limits)
-    .map(Number)
-    .sort((a, b) => b - a);
+  // If the specified amount cannot be decomposed into specified banknotes
+  if (minCoins[money] === Number.MAX_VALUE) { return []; }
 
-  return collect(ammountRequired, nominals);
-};
+  // minCoins - stores the minimum number of bills that are needed to issue a given amount
+  let sum = money;
+  while (sum > 0) {
+      let curSum = sum;
+      for (let coin = coins.length - 1; coin >= 0; coin--) {
+          let isCoinExist = coins[coin].count > 0;
+          if (isCoinExist && sum >= coins[coin].val && (minCoins[sum] == minCoins[sum - coins[coin].val] + 1 || minCoins[sum] == minCoins[sum - coins[coin].val])) {                                  
+              if (!needCoins[coin]) {
+                  needCoins[coin] = 0;
+              }
+              ++needCoins[coin];  
+              sum -= coins[coin].val;
+              coins[coin].count -= 1;               
+              break;
+          }
+      }
 
-getBtn.addEventListener('click', () => {
-  const getAmount = document.querySelector('.amount').value;
-  console.log(iWantToGet(getAmount, limits));
-});
+      if (curSum === sum) {
+          needCoins = [];
+          break; // not enough bills
+      }
+  }
+  return needCoins;
+}
+
+let coins = [ 
+  { val: 30, count: 10 }, 
+  { val: 50, count: 10 },
+  { val: 100, count: 10 },
+  { val: 500, count: 10 },
+  { val: 1000, count: 10 }
+];
+
+function enterAmount() {
+const amount = document.getElementById("amount").value;
+console.log(coins);
+const needCoins = giveMyMoney(amount, coins);
+let res = "", controlSum = 0;
+
+if (needCoins.length) {
+    needCoins.forEach((item, ind) => {
+      controlSum += item*coins[ind].val;
+      const symbol = ind < needCoins.length-1 ? " + " : "";
+       res += (item + "*" + coins[ind].val + symbol);
+    });
+}
+
+alert (res ? `${amount} = ${res}` : "Wrong amount entered or not enough bills in ATM");
+
+}
+
+const getMoney = document.getElementById("get-money")
+getMoney.addEventListener("click", enterAmount);
